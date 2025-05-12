0de85285f48aac7d54448f715977925cbe7e8679
GabiCdings <Gabicodings@gemail.com>
2025-05-11 21:16:58 -0400

    projeto html e css concluido

---
+++ Logica/app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1')
+let paragrafo = document.querySelector('p')
+
+titulo.innerHTML = 'Número Secreto'
+paragrafo.innerHTML = 'Escolha um numero entre 1 e 10'
+
+function verificarChute() {
+    console.log('O botão foi clicado')
+}
